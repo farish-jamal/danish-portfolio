dfd6457e0b3138e1dc4def8b1d468273ad91ffca
farish <developer.websites1@gmail.com>
2021-11-10 20:37:47 +0530

    about section added

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './about.css'
+
+function About() {
+ return (
+  <div className="about">
+     <div className="about__info">
+       <h2>Hey, I am Danish Jamal</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque earum at dolorum repellat nesciunt voluptas assumenda velit quidem facere! In odit, reiciendis quos dignissimos fugit rem odio eum qui. Blanditiis iste illo corrupti hic quas dicta nisi tempore. Quas quia sapiente fuga reprehenderit corrupti laudantium labore, nobis modi hic perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur accusamus non soluta esse exercitationem voluptates in quam ea! Iste beatae eos mollitia.</p>
+     </div>
+     <div className="about__photo">
+       <img src="https://farishjamal.online/image/Main_img%20(1).jpg" alt="photo_p" />
+     </div>
+  </div>
+ )
+}
+
+export default About
