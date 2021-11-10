9841c0b00105dea8e5c3f0394959a11263b5ecec
farish <developer.websites1@gmail.com>
2021-11-10 22:22:11 +0530

    header icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,10 +10,10 @@ function Header() {
      </div>
      <div className="header__right">
        <div className="header__right__links">
-         <a href="/">About Me</a>
-         <a href="/">Projects</a>
-         <a href="/">Skills</a>
-         <a href="/">Connect</a>
+       <a href="https://farishjamal.online"><i class="fa fa-github" aria-hidden="true"></i> </a>
+       <a href="/"><i class="fa fa-instagram" aria-hidden="true" style={{color:"#8a3ab9"}}></i> </a>
+       <a href="/"><i class="fa fa-twitter" aria-hidden="true" style={{color:"#0e76a8"}}></i> </a>
+       <a href="/"><i class="fa fa-linkedin" aria-hidden="true" style={{color:"#4267B2"}}></i> </a>
        </div>
        <button className="header__btn">Admin</button>
      </div>
